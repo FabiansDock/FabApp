279f491df35c0d2b51a8c1fec95b3d43b25da7c1
FabiansDock <fabian20122001@gmail.com>
2024-02-08 23:20:31 +0530

    Created drawing line

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.metrics import dp
 from kivy.properties import Clock
 from kivy.uix.widget import Widget
-from kivy.graphics.vertex_instructions import Line, Rectangle, Ellipse
+from kivy.graphics.vertex_instructions import Ellipse, Line
 from kivy.graphics.context_instructions import Color
 
 class CanvasExample4(Widget):
@@ -14,12 +14,14 @@ class CanvasExample4(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         with self.canvas:
-            self.ellipse = Ellipse(pos=(dp(200), self.center_y), size=(dp(20), dp(20)))
-            # self.rect = Rectangle(pos=self.ellipse.pos, )
+            self.ellipse = Ellipse(pos=(self.first_letter_start_pos, self.center_y), size=(dp(20), dp(20)))
+            self.line = Line(points=(self.first_letter_start_pos, self.center_y, *self.ellipse.pos), width=4)
+            self.line2 = Line(points=(), width=4)
+            self.line3 = Line(points=(), width=4)
         Clock.schedule_interval(self.update, 1/10)
     def on_size(self, *args):
         self.ellipse.pos = (self.first_letter_start_pos, self.center_y)
-
+        self.line.points = (self.first_letter_start_pos, self.center_y, *self.ellipse.pos)
     def update(self, dt):
         x, y = self.ellipse.pos
         
@@ -32,12 +34,14 @@ class CanvasExample4(Widget):
                 self.text_height = y
             else: 
                 self.ellipse.pos = (self.first_letter_start_pos, self.center_y+(y-self.center_y)/2)
+                self.line2.points += self.ellipse.pos
                 self.repeat = 2
                 return
         if self.repeat == 2 and x+dp(10)+self.ellipse.size[0] <= dp(100)+self.first_letter_start_pos:
             x += dp(10)
         if self.repeat == 2 and x+dp(10)+self.ellipse.size[0] > dp(100)+self.first_letter_start_pos:
             self.ellipse.pos = (x, self.center_y)
+            self.line3.points += self.ellipse.pos
             self.repeat = 3
             return
 
@@ -48,6 +52,7 @@ class CanvasExample4(Widget):
             self.count += 1
         if self.repeat == 3 and y+dp(10)+self.ellipse.size[1] > dp(400):
             self.ellipse.pos = (x-dp(10)*self.count/2, y-dp(20)*self.count/2)
+            self.line3.points += self.ellipse.pos
             self.count = 0
             self.repeat = 4
             return
@@ -56,6 +61,7 @@ class CanvasExample4(Widget):
             x += dp(10)
         if self.repeat == 4 and x+dp(10)+self.ellipse.size[0] > dp(200)+self.first_letter_start_pos:
             self.ellipse.pos = (x-dp(10)*self.count/2, self.text_height)
+            self.line3.points += self.ellipse.pos
             self.repeat = 5
             return
         if self.repeat == 5 and x+dp(10)+self.ellipse.size[0] <= dp(200)+self.first_letter_start_pos:
@@ -63,6 +69,7 @@ class CanvasExample4(Widget):
             y -= dp(20)
         if self.repeat == 5 and x+dp(10)+self.ellipse.size[0] > dp(200)+self.first_letter_start_pos:
             self.ellipse.pos = (x, self.center_y)
+            self.line3.points += self.ellipse.pos
             self.count = 0
             self.repeat = 6
             return 
@@ -97,9 +104,13 @@ class CanvasExample4(Widget):
             else:
                 self.ellipse.pos = (x+dp(10)*self.count, y)
                 self.count = 0
-                self.repeat =0
+                self.canvas.remove(self.ellipse)
+                self.repeat = 0
                 return       
         self.ellipse.pos = (x, y)
+        if self.repeat == 1: self.line.points += self.ellipse.pos
+        elif self.repeat == 2: self.line2.points += self.ellipse.pos
+        else : self.line3.points += self.ellipse.pos
 
 
 class TheMainApp(App):
